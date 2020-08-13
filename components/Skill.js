29462b711d6c42ebdafdfa7461d0db8303d9afee
= <ryancat.dev@gmail.com>
2020-08-13 17:27:00 -0400

    removed skill experience level

--- components/Skill.js
+++ components/Skill.js
@@ -1,11 +1,8 @@
 const Skill = ({ title, image, experience, className }) => {
   return (
-    <div className={`flex ${className}`}>
+    <div className={`flex items-center ${className}`}>
       <img src={image} className="w-20 h-20" />
-      <div className="text-left ml-5">
-        <h1 className="text-3xl font-light text-blue-500">{title}</h1>
-        <h3>{experience}</h3>
-      </div>
+      <h1 className="text-3xl ml-5 font-light">{title}</h1>
     </div>
   );
 };
